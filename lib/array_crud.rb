74e53e5bcd5071370dff417f9d97a1aa30cebec4
jamelia harrison <jameliaharrison@gmail.com>
2020-03-27 22:48:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,8 +2,8 @@ def create_an_empty_array
   []
 end
 
-def create_an_array [Christopher, Judith, Jamelia, Bradley] 
-  
+def create_an_array 
+  [Christopher, Judith, Jamelia, Bradley] 
 end
 
 def add_element_to_end_of_array(array, element)
